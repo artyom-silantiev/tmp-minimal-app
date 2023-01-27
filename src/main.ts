0e93fe8a1fca3109cced68565d475346d9f604d8
Artyom Silantiev <artyom.silantiev@gmail.com>
2023-01-27 12:17:03 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import bodyParser from 'body-parser';
+import { createAppLogger } from './app-logger';
 import { Applaction } from "./core/applaction";
 import { useCronService } from './core/cron';
 import { CronService } from './cron.service';
 import { useEnv } from './env/env';
 import routes from './routes';
+
+const logger = createAppLogger('App');
 
 function bootstrap() {
   const env = useEnv();
@@ -17,7 +20,9 @@ function bootstrap() {
   app.setRoutes(routes);
 
   app.listet(env.NODE_PORT, () => {
-    console.log(`app listen port: ${env.NODE_PORT}`);
+    logger.debug('dev env used');
+    logger.log('env: ', env);
+    logger.log(`app listen port: ${env.NODE_PORT}`);
   });
 
   useCronService(new CronService);
